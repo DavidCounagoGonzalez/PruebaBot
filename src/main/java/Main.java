bf8c1354a374e1e293cb30048ae01fb52a34dff5
David Counago Gonzalez <david.mouriscados@gmail.com>
2022-04-06 13:10:47 +0200

    Subido bot con algunos comandos base.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,14 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import discord4j.rest.util.Color;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.time.Instant;
 
 public class Main {
     public static void main (final String[]args ){
@@ -11,21 +19,68 @@ public class Main {
 
         //Se registra el token  para poder usarse en el login en Discord
 
-        String token = "OTUzNjMyOTk0NzgwOTA1NDcz.YjHZ7w.AYIdb1tgBD2LiOllGUfchPohhxw";
+        final String token = args[0];
         final DiscordClient cliente = DiscordClient.create(token);
         final GatewayDiscordClient puerto = cliente.login().block();
             //Se crea una clase y se implementa al bot.
-        assert puerto != null;
+
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                .color(Color.PINK)
+                .title("VLC")
+                .image("attachment://kirbyCono.png")
+                .build();
+
         puerto.on(MessageCreateEvent.class).subscribe(event -> {
             //Y se crea el evento de recibir un mensaje
             final Message message = event.getMessage();
             // se crea una igualdad con el mensaje recibido y buscando el canal en el que se escribió se devuelve el mensaje en ese canal.
             if ("!Hambre".equals(message.getContent())) {
                 final MessageChannel channel = message.getChannel().block();
-                assert channel != null;
-                channel.createMessage("Toma un melocotón <3").block();
+
+                channel.createMessage("Toma unos melocotones <3").block();
             }
+
+            if("!Help".equals(message.getContent())){
+                 String IMAGE_URL ="https://c.tenor.com/HG5nbp_7N_QAAAAd/triangle-shape.gif";
+                 String ANY_URL = "https://www.youtube.com/watch?v=yT16_7Jizn8&ab_channel=Zenuxio";
+                final MessageChannel channel = message.getChannel().block();
+                EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
+                builder.author("Yo", ANY_URL, IMAGE_URL);
+                builder.image(IMAGE_URL);
+                builder.title("Info");
+                builder.url(ANY_URL);
+                builder.description("Descripcion\n" +
+                        "No lo se\n" +
+                        "Solo tengo melocotones\n"+
+                        "!Hambre : para recibir melocotones \n" +
+                        "!Kirby : pruebalo ");
+                builder.thumbnail(IMAGE_URL);
+                builder.footer("setFooter --> setTimestamp", IMAGE_URL);
+                builder.timestamp(Instant.now());
+                channel.createMessage(builder.build()).block();
+            }
+
+            if ("!Kirby".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+
+                InputStream fileAsInputStream = null;
+                try {
+                    fileAsInputStream = new FileInputStream("C:\\Users\\david\\Downloads\\kirbyCono.png");
+                } catch (FileNotFoundException e) {
+                    System.out.println("Error lectura de fichero" + e.getMessage());
+                }
+                ;
+                channel.createMessage(MessageCreateSpec.builder()
+                        .content(" ")
+                        .addFile("kirbyCono.png", fileAsInputStream)
+                        .addEmbed(embed)
+                        .build()).subscribe();
+            }
+
+
         });
+
+
 
         puerto.onDisconnect().block();
     }
